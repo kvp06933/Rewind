23baa9576725938bbeca19e061ef687d055860bb
Katherine Pitts <kvp06933@gmail.com>
2021-10-05 13:41:54 -0500

    created rewind

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# projecttemplate
+# Retrograde
  Template for projects made with JS and Bootstrap
+
+ --A string is an array of characters
--- js/site.js
+++ js/site.js
@@ -1,20 +1,41 @@
 
 
-//display a message to the user
+//get starting values from the screen
+//Controller Function
 
-function displayMessage(){
-    //declare variable
-    let msg = "";
-    //pull the value from the input box
-    msg = document.getElementById("message").value;
+function getString(){
+    //get the user's input
+    let userString = document.getElementById("message").value;
+    //version 2 - test the user's input for length
     
-    alert(msg);
+
+    //Reverse the user's input - this is a separate function
+    let revString = reverseString(userString);
+    //Display the result - this is a separate function
+    displayRetrograde(revString);
+    
    
 }
 
-function displayMessage2(){
-    let msg = "";
-    msg = document.getElementById("message").value;
-    Swal.fire(msg);
+//logic function
+ function reverseString(userString){
+    let startValue = userString.length-1;
+    let endValue = 0;
+    let revString = "";
+    //User input = Frank
+    //Frank is a string AND an array of characters
+    //In a decremented for loop the startValue must be higher than the endValue
+    for (let index = startValue; index >= endValue; index--) {
+        revString += userString[index];
+        
+    }
+    return revString;
+ }
+//display function
+ function displayRetrograde(revString){
+    let output = document.getElementById("results");
 
-}
+    output.innerHTML = revString; // This replaces/overwrites the innerHTML with revString
+ }
+
+
